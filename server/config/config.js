df0e70dd9d54cda1c8fffa654b7cd1a60e9d0612
Ricardo <ricardovalenzuelaquintana7@gmail.com>
2022-03-24 14:55:20 -0700

    cambio config para env

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'deV';
 //Base de datos
 //===========================
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://ricardobdcls:1a1s1d1f@cluster0.dkyyo.mongodb.net/test';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://ricardobdcls:1a1s1d1f@cluster0.dkyyo.mongodb.net/test';
+}
 
 process.env.URLDB = urlDB;
