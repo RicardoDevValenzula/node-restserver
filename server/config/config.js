4649f5bc48679698a4c76d6737f15cba13034efb
Ricardo <ricardovalenzuelaquintana7@gmail.com>
2022-03-24 15:17:52 -0700

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ricardobdcls:1a1s1d1f@cluster0.dkyyo.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
